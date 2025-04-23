976f447f12302089974240c0953e0154c7f21f5b
nischal1101 <kaflenk123@gmail.com>
2025-04-23 22:39:45 +0545

    setup expo with nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-5xl bg-red-400">hi</Text>
+    </View>
+  );
+}
