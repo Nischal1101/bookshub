1334310b7b223fd85134f5380ed0fece47582a34
nischal1101 <kaflenk123@gmail.com>
2025-04-30 09:06:17 +0545

    add logout for debugging purpose

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import BookCard from "@/components/book-card";
+import { useAuthStore } from "@/store/authStore";
 
 export default function Home() {
+  const { logout } = useAuthStore();
   return (
     <View className="gap-2 mt-8 ">
       <View className="flex-row justify-center gap-1">
         <Text className="text-4xl text-primary ">BooksHub</Text>
+
         <Ionicons size={34} name="glasses-outline" color="#4CAF50" />
       </View>
       <Text className="text-textSecondary mx-auto text-lg ">
         Discover great reads from the community.{" "}
       </Text>
+      <TouchableOpacity onPress={logout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
       <BookCard />
     </View>
   );
